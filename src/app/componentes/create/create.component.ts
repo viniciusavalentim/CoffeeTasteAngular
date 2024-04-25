3b9424f573612c445a0610f2e0b56dbd6781bb9f
Vinicius <viniciusvalentim769@gmail.com>
2024-04-25 18:07:50 -0300

    Criando o formulário para cadastro das bebidas geladas

--- src/app/componentes/create/create.component.ts
+++ src/app/componentes/create/create.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
 
+  drinksForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+  ngOnInit(): void {
+    this.drinksForm = this.fb.group({
+      name: ['', Validators.required],
+      observacoes: ['', Validators.required],
+      ingredientes: this.fb.array([])
+    });
+  };
+
+  get ingredients(): FormArray{
+    return this.drinksForm.get('ingredientes') as FormArray;
+  };
+
+  AddDrink(){
+    this.ingredients.push(this.fb.group({
+      name: ['', Validators.required],
+      quantity: ['', Validators.required],
+      unit: ['', Validators.required],
+    }));
+  };
+
+  submit(){
+
+  };
 }
